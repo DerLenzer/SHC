ef9dce808fea694495d09c98a3bfd4a935aa6dd5
DerLenzer <123672964+DerLenzer@users.noreply.github.com>
2024-03-26 08:04:01 -0400

    Update password_cracker.sh

--- password_cracker.sh
+++ password_cracker.sh
@@ -7,7 +7,7 @@ password_list_path="./10k-most-common.txt"
 username="anna"
 
 # Anzahl der Prozesse, die gleichzeitig gestartet werden sollen
-num_processes=10
+num_processes=5
 
 # Funktion, um Passwörter zu testen
 check_password() {
